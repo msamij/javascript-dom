890070dd76f6bab564d35d4112967a9c569a498e
Muhammad Sami <msamiaj@outlook.com>
2025-06-03 07:46:00 +0500

    scrip2.js

---
+++ script2.js
@@ -0,0 +1,44 @@
+let quit = false;
+const numbers = [];
+
+// while (!quit) {
+//   let input = +prompt('Enter a number (Press 0 to quit!):');
+//   if (input < 0) {
+//     alert('Please enter positive number');
+//     continue;
+//   } else if (input === 0) {
+//     quit = true;
+//   } else {
+//     numbers.push(input);
+//   }
+// }
+
+// console.log(numbers);
+
+// for (const element of numbers) {
+//   if (element < 10) continue;
+//   console.log(element);
+// }
+
+let students = {
+  John: {
+    grade: 80,
+  },
+  Peter: {
+    grade: 90,
+  },
+  Micheal: {
+    grade: 75,
+  },
+};
+
+// let studentName = prompt('Enter student name:');
+// if (studentName in students) console.log(`${studentName} grade is ${students[studentName].grade}`);
+// else console.log('Student name not found in database!');
+
+let fruits = ['mango', 'apple', 'bannana'];
+
+// splice returns the array containing the deleted elements.
+let newFruits = fruits.splice(1, 1, 'orange');
+console.log(newFruits);
+console.log(fruits);
